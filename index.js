576b56c6d33b3e9bc3dd98e8363a78b34fdc4de2
Lillian Jensen <ljense251@west-mec.org>
2024-08-14 14:13:37 -0700

    first line

--- index.js
+++
@@ -1,5 +0,0 @@
-// import requirements
-
-function main() { }
-
-global.main = main;
--- utils/alienGravityFactors.js
+++
@@ -1,6 +0,0 @@
-const alienFactors = {
-    Glarp: 1.234,
-    Vulcan: 2.225,
-    Krypton: 0.567
-}
-module.exports = alienFactors;
